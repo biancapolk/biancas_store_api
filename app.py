ffe8183f2bc49084a7813dbfed763659198583ae
Bia Polk <bianca@shipwell.com>
2021-05-27 16:00:15 -0500

    cleaned up files

--- app.py
+++ app.py
@@ -11,30 +11,20 @@ from resources.store_resource import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Turning off Flask SQL Alchemy Tracker because SQL Alchemy, the main library, has its own tracking
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)  # /auth
+jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')  # When we execute a post request to /register it will call the UserRegister class
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-api.add_resource(UserRegister, '/register')
-
-if __name__ == '__main__':
+if __name__ == '__main__':  # Q: This allows us to import app without running app.run()
     from db import db
-
     db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+    app.run(debug=True)  # important to mention debug=True
