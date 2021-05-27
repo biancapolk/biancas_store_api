023f8dc716873ea97b3fd385d33be84fb7b818ae
Bia Polk <bianca@shipwell.com>
2021-05-27 15:40:08 -0500

    reformatted file corrected misspellings and cleaned up app.py

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ from resources.store_resource import Store, StoreList
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config[
-    'SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Turning off Flask SQL Alchemy Tracker because SQL Alchemy, the main library, has its own tracking
-app.secret_key = 'jose'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
@@ -26,6 +24,6 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-if __name__ == '__main__':  # Q: This allows us to import app without running app.run()
+if __name__ == '__main__':
     db.init_app(app)
-    app.run(debug=True)  # important to mention debug=True
+    app.run(debug=True)
